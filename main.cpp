5a326384178881ab2619b470a99108f7f81a85c3
fxstd <fx1213@qq.com>
2016-01-06 22:49:04 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ class CHttpTest : public CHttpCurl
 int main()
 {
     std::string posturl = "https://accounts.google.com/o/oauth2/token";
-    std::string postfileds = "grant_type=refresh_token&client_id=client_id&client_secret=client_secret&refresh_tok=refresh_token";
+    std::string postfileds = "grant_type=refresh_token&client_id=client_id&client_secret=client_secret&refresh_token=refresh_token";
     CHttpCurl::CurlGlobalInit();
     CHttpTest ht;
     ht.Post(posturl,postfileds);
