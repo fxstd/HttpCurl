5a6ed95cba45f92b02d7535dbdf98f44f0ef88bf
fxstd <fx1213@qq.com>
2016-01-12 16:22:09 +0800

    Update HttpCurl.h
    增加https线程安全

--- HttpCurl.h
+++ HttpCurl.h
@@ -1,7 +1,26 @@
 
 #pragma once
 
+//指定加密库
+#define USE_OPENSSL
+//#define USE_GNUTLS
+
+#include <string>
 #include <curl/curl.h>
+
+#ifdef USE_OPENSSL
+#include <pthread.h>
+#include <openssl/crypto.h>
+static pthread_mutex_t* lockarray;
+#endif
+
+#ifdef USE_GNUTLS
+#include <pthread.h>
+#include <gcrypt.h>
+#include <errno.h> 
+GCRY_THREAD_OPTION_PTHREAD_IMPL;
+#endif
+
 
 class CHttpCurl
 {
@@ -19,13 +38,62 @@ class CHttpCurl
 		static CURLcode CurlGlobalInit()
 		{
 			CURLcode m_curlCode = curl_global_init(CURL_GLOBAL_ALL);
+			init_locks();
 			return m_curlCode;
 		}
 		//清理 curl环境 只能调用1次
 		static void CurlGlobalCleanUp()
 		{
+			kill_locks();
 			curl_global_cleanup();
 		}
+#ifdef USE_OPENSSL
+		static void lock_callback(int mode, int type, char *file, int line)
+		{
+			(void)file;
+			(void)line;
+			if (mode & CRYPTO_LOCK) {
+				pthread_mutex_lock(&(lockarray[type]));
+			}
+			else {
+				pthread_mutex_unlock(&(lockarray[type]));
+			}
+		}
+		static unsigned long thread_id(void)
+		{
+			unsigned long ret;
+			ret = (unsigned long)pthread_self();
+			return(ret);
+		}
+		static void init_locks()
+		{
+			lockarray = (pthread_mutex_t *)OPENSSL_malloc(CRYPTO_num_locks() * sizeof(pthread_mutex_t));
+			for(int i = 0; i < CRYPTO_num_locks(); i++) 
+			{
+				pthread_mutex_init(&(lockarray[i]),NULL);
+			}
+			CRYPTO_set_id_callback((unsigned long (*)())thread_id);
+			CRYPTO_set_locking_callback((void (*)(int, int, const char*, int))lock_callback);
+		}
+		static void kill_locks()
+		{
+			CRYPTO_set_locking_callback(NULL);
+			for(int i = 0; i < CRYPTO_num_locks(); i++)
+			{
+				pthread_mutex_destroy(&(lockarray[i]));
+			}
+			OPENSSL_free(lockarray);
+		}
+#endif
+#ifdef USE_GNUTLS
+		void init_locks()
+		{
+			gcry_control(GCRYCTL_SET_THREAD_CBS);
+		}
+		void kill_locks()
+		{
+		}
+#endif
 		static size_t rev_data(void *ptr, size_t size, size_t nmemb, void *stream)
 		{
 			CHttpCurl* pHttp = (CHttpCurl*)stream;
@@ -86,15 +154,15 @@ class CHttpCurl
 			return m_curlCode;
 		}
 		//设置附带数据
-	        void SetExtra(void* extra)
-	        {   
-	            m_extra = extra;
-	        }   
-	        //获取附带数据
-	        void* GetExtra()
-	        {   
-	            return m_extra;
-	        } 
+		void SetExtra(void* extra)
+		{
+			m_extra = extra;
+		}
+		//获取附带数据
+		void* GetExtra()
+		{
+			return m_extra;
+		}
 		//post/get 请求的相应函数
 		//response 收到的相应数据
 		virtual void Response(std::string& response) = 0;
@@ -103,4 +171,3 @@ class CHttpCurl
 		CURL* m_curl;
 		void* m_extra;
 };
-
